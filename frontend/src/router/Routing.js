c9c0bb2be01a1465d4a6c63167bc2299af28a98e
Raul Hernandez <devrulls@gmail.com>
2022-05-26 22:29:40 +0200

    frontend user

--- frontend/src/router/Routing.js
+++ frontend/src/router/Routing.js
@@ -4,6 +4,8 @@ import {Error} from "../components/Error";
 import {HomeScreen} from "../screens/HomeScreen";
 import {ProductScreen} from "../screens/ProductScreen";
 import {CartScreen} from "../screens/CartScreen";
+import {LoginScreen} from "../screens/LoginScreen";
+
 
 
 export const Routing = () => {
@@ -11,6 +13,7 @@ export const Routing = () => {
             <Routes>
                 <Route path="/" element={<Navigate to="/home"/>}/>
                 <Route path="/home" element={<HomeScreen/>}/>
+                <Route path="/login" element={<LoginScreen/>}/>
                 <Route path="/product/:id" element={<ProductScreen/>}/>
                 <Route path="/cart/:id" element={<CartScreen/>}/>
                 <Route path="/cart" element={<CartScreen/>}/>
