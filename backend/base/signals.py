539144c0c14c0d574f6e0c161bdd662c86f9750a
Raul Hernandez <devrulls@gmail.com>
2022-05-26 23:12:36 +0200

    Backend - fix signals file

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -5,8 +5,9 @@ from django.db.models.signals import pre_save
 def updateUser(sender, instance, **kwargs):
     print('Signals Triggered')
     user = instance
-    if user.email is not "":
+    if user.email != "":
         user.username = user.email
 
 
 pre_save.connect(updateUser, sender=User)
+
